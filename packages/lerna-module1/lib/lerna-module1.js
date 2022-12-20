a14f59f8a6357d9b9e727e8ab600be43a0fee22f
zl-mdc <1424084297@qq.com>
2022-12-20 22:04:22 +0800

    four commit

--- packages/lerna-module1/lib/lerna-module1.js
+++ packages/lerna-module1/lib/lerna-module1.js
@@ -1,6 +1,7 @@
 // lerna-module1/lib/lerna-module1.js
 module.exports = lernaModule1;
 function lernaModule1() {
+    console.log('demo11');
   console.log('lerna-module1');
   console.log("demo1");
 }
